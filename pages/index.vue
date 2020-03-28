0aab68ef0f4610b31a0d047a107c4afc29e38fd9
bengineer19 <benjames1999@hotmail.co.uk>
2020-03-28 15:53:05 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <noscript>
+    <TestComp></TestComp>
+  </noscript>
+</template>
+
+<script>
+import TestComp from "~/components/TestComp.vue";
+
+export default {
+  components: {
+    TestComp
+  }
+};
+</script>
